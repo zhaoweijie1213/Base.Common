7a5a5b196b75ac8f4fee0d64447d3bda78620a83
zhaoweijie <zhaoweijie1108@qq.com>
2025-04-03 11:35:48 +0800

    添加全局公共路径

---
+++ QYQ.Base.Common/GobalCommonObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QYQ.Base.Common
+{
+    /// <summary>
+    /// 全局公共对象
+    /// </summary>
+    public class GobalCommonObject
+    {
+        /// <summary>
+        /// windows密钥文件夹
+        /// </summary>
+        public const string WindowsKeyFolder = @"C:\Users\Default\AppData\Local\Data\config";
+
+        /// <summary>
+        /// linux密钥文件夹
+        /// </summary>
+        public const string LinuxKeyFolder = "/etc/ssl/private";
+
+
+        /// <summary>
+        /// 获取路径
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPath()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return WindowsKeyFolder;
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return LinuxKeyFolder;
+            }
+            else
+            {
+                throw new PlatformNotSupportedException();
+            }
+        }
+    }
+}
