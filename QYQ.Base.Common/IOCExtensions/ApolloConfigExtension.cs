a052d58d886943cb98c4cca9e066eae0f7d9613f
zhaoweijie <zhaoweijie1108@qq.com>
2026-04-22 11:32:26 +0800

    移除apollo集群判断，使用默认的

--- QYQ.Base.Common/IOCExtensions/ApolloConfigExtension.cs
+++ QYQ.Base.Common/IOCExtensions/ApolloConfigExtension.cs
@@ -1,12 +1,16 @@
-﻿using Com.Ctrip.Framework.Apollo.Logging;
-using Com.Ctrip.Framework.Apollo;
+﻿using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Logging;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Immutable;
 
 namespace QYQ.Base.Common.IOCExtensions
 {
@@ -15,6 +19,7 @@ namespace QYQ.Base.Common.IOCExtensions
     /// </summary>
     public static class ApolloConfigExtension
     {
+
         /// <summary>
         /// 添加apollo配置
         /// </summary>
@@ -22,20 +27,20 @@ namespace QYQ.Base.Common.IOCExtensions
         /// <returns></returns>
         public static IApolloConfigurationBuilder AddQYQApollo(this IConfigurationBuilder builder)
         {
-            LogManager.UseConsoleLogging(Com.Ctrip.Framework.Apollo.Logging.LogLevel.Information);
+            LogManager.UseConsoleLogging(Com.Ctrip.Framework.Apollo.Logging.LogLevel.Trace);
             var configuration = builder.Build();
             if (configuration == null)
             {
                 throw new ArgumentNullException(nameof(configuration), "配置不能为空");
             }
             //获取apollo配置
-            ApolloOptions apollo = configuration.GetSection("apollo").Get<ApolloOptions>()!;
-            //读取集群
-            if (!string.IsNullOrEmpty(configuration["Cluster"]))
-            {
-                apollo.Cluster = configuration["Cluster"]!;
-            }
-
+            ApolloOptions? apollo = configuration.GetSection("apollo").Get<ApolloOptions>() ?? throw new InvalidOperationException("Apollo 配置缺失或无效");
+            //var cluster = configuration.GetSection("apollo:Cluster").Get<string>();
+            ////读取集群
+            //if (!string.IsNullOrEmpty(cluster))
+            //{
+            //    apollo.Cluster = cluster;
+            //}
             //设置apollo的服务地址
             List<string> configServer = new()
             {
@@ -43,6 +48,30 @@ namespace QYQ.Base.Common.IOCExtensions
             };
             apollo.ConfigServer = configServer;
             return builder.AddApollo(apollo);
+        }
+
+        /// <summary>
+        /// 启用 Apollo 日志
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static WebApplication UseQYQApolloLogger(this WebApplication app)
+        {
+            var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
+            MelLogging.UseMel(loggerFactory);
+            return app;
+        }
+
+        /// <summary>
+        /// 启用 Apollo 日志
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IHost UseQYQApolloLogger(this IHost app)
+        {
+            var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
+            MelLogging.UseMel(loggerFactory);
+            return app;
         }
     }
 }
